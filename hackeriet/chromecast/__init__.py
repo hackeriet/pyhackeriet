51419a52b3b12670221f8f93d9ae0d2e8444617c
Stig Otnes Kolstad <stig@stigok.com>
2017-07-17 15:33:25 +0200

    Fix runtime exceptions
    - Set push MQTT topic with env var
    - Set chromecast target device name with env var
    - Fix runtime errors due to updated pychromecast dependency
    - Add systemd .service file for chromecast-snoop

--- hackeriet/chromecast/__init__.py
+++ hackeriet/chromecast/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-import json
+import json, os
 from hackeriet.mqtt import MQTT
+
+target_cast_name = os.getenv("CAST_NAME", "Hackeriet")
+mqtt_topic = os.getenv("MQTT_CAST_TOPIC", "hackeriet/chromecast")
 
 class Listener(object):
     def __init__(self):
@@ -14,21 +17,19 @@ class Listener(object):
             self.artist = status.artist
             self.title = status.title
             print("{} -- {}".format(status.artist, status.title))
-            #if status.artist != None or status.title != None:
             print(status.media_metadata)
-            self.mqtt("hackeriet/chromecast", json.dumps(status.media_metadata))
+            self.mqtt(mqtt_topic, json.dumps(status.media_metadata))
 
 def snoop():
     import pychromecast, time
 
-    cast = pychromecast.get_chromecast()
-    if cast is None: # shrug emoji
-        cast = pychromecast.Chromecast("192.168.100.2")
+    chromecasts = pychromecast.get_chromecasts()
+    cast = next(cc for cc in chromecasts if cc.device.friendly_name == target_cast_name)
     cast.wait()
     print("Connected to {}".format(cast.device.friendly_name))
     cast.media_controller.register_status_listener(Listener())
     while True:
-        time.sleep(30)
+        time.sleep(15)
 
 if __name__ == "__main__":
     snoop()
