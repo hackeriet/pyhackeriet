6140790ba5e7439932942baed6e6c4bea355ac41
Stig Otnes Kolstad <stig@stigok.com>
2017-07-29 17:21:37 +0200

    More logging

--- hackeriet/cardreaderd/__init__.py
+++ hackeriet/cardreaderd/__init__.py
@@ -13,20 +13,25 @@ door_timeout = int(os.getenv("DOOR_TIMEOUT", 2))
 mqtt = MQTT()
 
 def main():
+  logging.debug('Starting main loop')
   while True:
     users.load()
     # Read data from card reader
+    logging.debug('mifare: waiting for data...')
     data = mifare.try_read()
     if data:
+      logging.debug('mifare: data read')
       user = users.auth(data[0:16])
       if user:
         ascii_user = user.encode('ascii', 'replace').decode('ascii')
-        logging.info('Card reader read card for %s' % ascii_user)
+        logging.info('auth: card read for user %s' % ascii_user)
         mqtt(door_topic, user)
       else:
-        logging.debug('User not found: %s' % data[0:16])
+        logging.debug('auth: card data does not belong to a user: %s' % data[0:16])
       # Avoid spewing messages every single ms while a card is in front of the reader
       time.sleep(door_timeout)
+    else
+      logging.debug('mifare: no data read in last attempt')
 
 if __name__ == "__main__":
   main()
