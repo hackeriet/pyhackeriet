c693b3090754fb7298dae74f30a6bc127a605893
Stig Otnes Kolstad <stig@stigok.com>
2019-06-13 23:51:05 +0200

    brusweb: Re-order CREATE TABLE statements to avoid errors

--- hackeriet/web/brusweb/sql/2.sql
+++ hackeriet/web/brusweb/sql/2.sql
@@ -1,14 +1,14 @@
 CREATE TABLE machine (machine TEXT PRIMARY KEY,
   key TEXT);
 
+CREATE TABLE product (pid INTEGER PRIMARY KEY,
+  product TEXT,
+  price INTEGER);
+
 CREATE TABLE slot_product (spid INTEGER PRIMARY KEY,
   slot INTEGER,
   machine TEXT references machine,
   product INTEGER references product);
-
-CREATE TABLE product (pid INTEGER PRIMARY KEY,
-  product TEXT,
-  price INTEGER);
 
 ALTER TABLE transactions ADD machine TEXT references machine;
 ALTER TABLE transactions ADD product INTEGER references product;
